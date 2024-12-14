38a901f902fb97f4e1421ebe6dac1b8cecd6c5f0
Rinshad Choorappara <101931172+tinkvu@users.noreply.github.com>
2024-12-14 15:05:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from gtts import gTTS
 from pydub import AudioSegment
 import streamlit as st
 from audiorecorder import audiorecorder
-from IPython.display import Audio
 
 # Initialize the Groq client
 API_KEY = "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"
@@ -37,10 +36,12 @@ def play_audio_with_gtts(text, output_file="output_audio.mp3"):
 def translate_to_english(text):
     completion = client.chat.completions.create(
         model="llama3-8b-8192",
-        messages=[{
-            "role": "system",
-            "content": f"translate this to english: {text}\nOnly output the translation."
-        }],
+        messages=[
+            {
+                "role": "system",
+                "content": f"translate this to english: {text}\nOnly output the translation."
+            },
+        ],
         temperature=1,
         max_tokens=1024,
         top_p=1,
@@ -56,12 +57,9 @@ def translate_to_english(text):
     return translation.strip()
 
 # Streamlit App
-st.title("Audio Transcription and Translation")
+st.title("Any Language to English")
 
-# Option to record or upload audio
-st.subheader("Choose an option to provide audio:")
-option = st.radio("Select input method:", ("Upload Audio", "Record Audio"))
-
+st.write("Check the word pronunciations here!")
 # Text input for pronunciation
 pron = st.text_input("Enter the text to be pronounced:")
 
@@ -69,6 +67,13 @@ if pron:
     # Play the entered text as speech
     audio_output = play_audio_with_gtts(pron)
     st.audio(audio_output, format="audio/mp3", autoplay=True)
+
+
+# Option to record or upload audio
+st.subheader("Choose an option to provide audio:")
+option = st.radio("Select input method:", ("Upload Audio", "Record Audio"))
+
+
 
 if option == "Upload Audio":
     uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
